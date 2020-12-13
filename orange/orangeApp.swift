2d96bbcc5e59a49d3df2bb90e6ee34b8d8ac204b
mfileman <madisonfileman@gmail.com>
2020-12-13 00:47:53 -0500

    initial commit :^)

---
+++ orange/orangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  orangeApp.swift
+//  orange
+//
+//  Created by madison on 12/13/20.
+//
+
+import SwiftUI
+
+@main
+struct orangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
